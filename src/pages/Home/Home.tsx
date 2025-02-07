e0f0ee0fb45001c5808cb28507fae338c064503e
MijaelYG <yauyogranadosmijael@gmail.com>
2025-02-07 02:08:19 -0500

    efecto en btn comenzar, se añadio btn skip

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,11 +4,15 @@ import styles from "./Home.module.css";
 const Home = () => {
   const [play, setPlay] = useState(false);
   const [opacity, setOpacity] = useState(1);
+  const [btnclick, setBtnclick] = useState(true);
   const videoRef = useRef<HTMLVideoElement>(null!);
 
   const handlePlay = () => {
     if (play == false) {
       setPlay(true);
+      setTimeout(() => {
+        setBtnclick(false);
+      }, 600);
       videoRef.current.play();
       setOpacity(0);
     } else {
@@ -19,20 +23,24 @@ const Home = () => {
   return (
     <>
       <div className={styles.intro}>
-        <div style={{ opacity: opacity }} className={styles.intro_play}>
-          <div className={styles.intro__play__gif}>
-            <img src="/videos/gif_isaac_intro.gif" alt="" />
-          </div>
-          <div className={styles.intro__play__btn}>
-            <div className={styles.intro__play__btn__gif}>
-              <img src="/img/UI/title_streak.png" alt="" />
+        {btnclick && (
+          <div style={{ opacity: opacity }} className={styles.intro_play}>
+            <div className={styles.intro__playgif}>
+              <img src="/videos/gif_isaac_intro.gif" alt="" />
             </div>
-            <button onClick={handlePlay} disabled={play == true}>
-              COMENZAR
-            </button>
+            <div className={styles.intro__playbtn}>
+              <button onClick={handlePlay} disabled={play == true}>
+                COMENZAR
+              </button>
+              <div className={styles.titleui}>
+                <img src="/img/UI/title_streak.png" alt="" />
+              </div>
+            </div>
           </div>
-        </div>
+        )}
         <video ref={videoRef} src="/videos/intro.mp4"></video>
+        <div className={styles.btn_skip}>
+          <button>SKIP</button> </div>
       </div>
       <div className=""></div>
     </>
