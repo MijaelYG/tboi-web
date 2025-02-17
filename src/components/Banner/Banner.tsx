0a7e0a444b65d153d2afb66813d7d7f5d073e3c2
MijaelYG <yauyogranadosmijael@gmail.com>
2025-02-16 19:24:08 -0500

    agregando efecto scroll a botones

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,7 +1,23 @@
 import { motion } from "framer-motion";
 import style from "./Banner.module.css";
+import { useEffect, useRef } from "react";
 
 const Banner = () => {
+
+  const  BtnExp = useRef<HTMLDivElement | null>(null)
+  const  BtnJu = useRef<HTMLDivElement | null>(null)
+  useEffect(() => {
+    const handleScroll = () =>{
+      if(BtnExp.current !== null && BtnJu.current !==null){
+        BtnExp.current.style.opacity = window.scrollY > 150 ? "1": "0"
+      }
+    }
+
+    window.addEventListener("scroll",handleScroll);
+    return () => (window.removeEventListener("scroll", handleScroll))
+  }, [])
+  
+
   return (
     <div className="container">
         <div className={style.banner}>
@@ -16,15 +32,14 @@ const Banner = () => {
                 <div className={style.fly}>
                 </div>
               </div>
-
-              <div className={style.text}>
+              <motion.div initial={{opacity:0, y: -10}} animate={{opacity:1, y:0}} transition={{duration:0.6, delay:0.9}} className={style.text}>
                 <p>"¡Explora los secretos ocultos de The Binding of Isaac! Sumérgete en un mundo lleno de misterios, desafíos y sorpresas mientras descubres estrategias avanzadas, desentrañas los secretos de cada personaje y desbloqueas poderosos ítems. Aprende a enfrentar a los enemigos más temibles y conviértete en un verdadero veterano."</p>
-              </div>
+              </motion.div>
               <div className={style.btn_info}>
-                <div className={style.btn_expl}>
+                <div ref={BtnExp} className={style.btn_expl}>
                   <img src="/img/UI/explorar.png" alt="" />
                 </div>
-                <div className={style.btn_expl}>
+                <div ref={BtnJu} className={style.btn_expl}>
                 <img src="/img/UI/jugar.png" alt="" />
                 </div>
               </div>
