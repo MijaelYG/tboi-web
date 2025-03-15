018010e887572e4c015d00bc511f280743f12cef
MijaelYG <yauyogranadosmijael@gmail.com>
2025-03-14 22:43:18 -0500

    cambios pequeños

--- src/components/HomeSectionInfos/SectionInfoTwo/SectionInfoTwo.tsx
+++ src/components/HomeSectionInfos/SectionInfoTwo/SectionInfoTwo.tsx
@@ -3,25 +3,25 @@ import styles from "./SectionInfoTwo.module.css";
 const SectionInfoTwo = () => {
   return (
     <div className={styles.container_section_info}>
-      <div className={styles.card_one}>
+      <div className={styles.card}>
         <div className={styles.image_h}></div>
         <div className={styles.title}></div>
         <div className={styles.desc}>
-        Isaac vivía feliz con su madre en una pequeña casa en lo alto de una colina. Pasaba sus días dibujando y jugando, mientras su madre veía programas religiosos en la televisión. Pero un día, ella escuchó una voz del cielo.
+        Isaac y su madre vivían en una pequeña casa en lo alto de una colina. Él pasaba sus días dibujando y jugando, mientras su madre veía programas religiosos. Todo era tranquilo, hasta que un día, ella escuchó una voz celestial.
         </div>
       </div>
-      <div className={styles.card_one}>
+      <div className={styles.card}>
         <div className={styles.image_h}></div>
         <div className={styles.title}></div>
         <div className={styles.desc}>
-        La voz le dijo que su hijo estaba corrompido por el pecado y que debía salvarlo. Al principio, su madre le quitó sus juguetes y ropas, aislándolo del mundo. Pero la voz insistió… su hijo debía ser sacrificado.
+        La voz le dijo que su hijo estaba manchado por el pecado y que debía ser purificado. Obediente, su madre tomó un cuchillo, decidida a cumplir la voluntad divina. Isaac, aterrorizado, vio cómo su destino se sellaba.
         </div>
       </div>
-      <div className={styles.card_one}>
+      <div className={styles.card}>
         <div className={styles.image_h}></div>
         <div className={styles.title}></div>
         <div className={styles.desc}>
-        Al ver a su madre entrar a la habitación con un cuchillo, Isaac entró en pánico. Miró a su alrededor y encontró una trampilla en el suelo. Sin otra opción, la abrió y saltó… descendiendo hacia lo desconocido.
+        Antes de que fuera demasiado tarde, Isaac encontró una trampilla en su habitación. Sin otra opción, la abrió y se lanzó al vacío, cayendo en un oscuro sótano lleno de peligros que nunca imaginó.
         </div>
       </div>
     </div>
