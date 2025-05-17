128b6bf7e2283b6aae7dd451e11e4d74462c47a1
MijaelYG <yauyogranadosmijael@gmail.com>
2025-05-17 01:31:10 -0500

    chore(header): mejora final del diseño de navegacion (falta agregar direcciones del url)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,75 +1,152 @@
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 
 const Header = () => {
+  const [btnClickHeader, setBtnClickHeader] = useState<null | number>(null);
+  const HandleBtnHeader = () => {
+    if (btnClickHeader == 0 || btnClickHeader == null) {
+      setBtnClickHeader(1);
+    } else {
+      setBtnClickHeader(0);
+    }
+  };
+
+  const variantsHeader = {
+    initial: {
+      x: "-28vw", opacity:0
+    },
+    visible: {
+      x: "0vw", opacity:1,
+      transition: { duration: 0.4, ease: "easeOut" }, 
+    },
+    visiblefirst: {
+      x: "-20vw", opacity:1,
+      transition: { duration: 0.6, delay:1.1, ease: "backOut" }, 
+    },
+    hidden: {
+      x: "-20vw", opacity:1,
+      transition: { duration: 0.4, ease: "easeOut" },
+    },
+  };
   return (
     <motion.header
-      initial={{ y: -100 }}
-      animate={{ y: 0 }}
-      transition={{ duration: 1 }}
       className="container"
+      custom={btnClickHeader}
+      variants={variantsHeader}
+      initial="initial"
+      animate={
+        btnClickHeader === 1
+          ? "visible"
+          : btnClickHeader === 0
+          ? "hidden"
+          : "visiblefirst"
+      }
+      transition={{ duration: 0.4, ease: "easeInOut" }}
     >
-      <div className={styles.header2}>
-        <div className={styles.cont_nav}>
-          <div className={styles.nav}>
-            <div className={styles.nav_btn}>
-              <Link to={"/"}> 
-                <img src="/img/nav/start_search.webp" alt="" /> Inicio
-              </Link>
-            </div>
-                        <div className={styles.nav_btn}>
-              <Link to={"/"}> 
-                <img src="/img/nav/start_search.webp" alt="" /> Inicio
-              </Link>
-            </div>
-                        <div className={styles.nav_btn}>
-              <Link to={"/"}> 
-                <img src="/img/nav/start_search.webp" alt="" /> Inicio
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className={styles.btn_header}></div>
+      <div className={styles.cont_header}>
+        <AnimatePresence mode="wait">
+          {btnClickHeader == 1 && (
+            <motion.div
+              className={styles.nav}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.25 }}
+            >
+              <motion.div
+                className={styles.nav_btn}
+                initial={{ x: -15, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.4, delay: 0.29, ease: "easeOut" }}
+              >
+                <Link to={"/"}>
+                  <img src="/img/nav/start_search.webp" alt="" />
+                  <span>Inicio</span>
+                </Link>
+              </motion.div>
+              <motion.div
+                className={styles.nav_btn}
+                initial={{ x: -15, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.4, delay: 0.4, ease: "easeOut" }}
+              >
+                <Link to={"/Items"}>
+                  <img src="/img/nav/item_search.webp" alt="" />
+                  <span>Items</span>
+                </Link>
+              </motion.div>
+              <motion.div
+                className={styles.nav_btn}
+                initial={{ x: -15, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.4, delay: 0.51, ease: "easeOut" }}
+              >
+                <Link to={""}>
+                  <img src="/img/nav/enemie_search.webp" alt="" />
+                  <span>Enemigos</span>
+                </Link>
+              </motion.div>
+              <motion.div
+                className={styles.nav_btn}
+                initial={{ x: -15, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.4, delay: 0.62, ease: "easeOut" }}
+              >
+                <Link to={""}>
+                  <img src="/img/nav/character_search.webp" alt="" />
+                  <span>Personajes</span>
+                </Link>
+              </motion.div>
+              <motion.div
+                className={styles.nav_btn}
+                initial={{ x: -15, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.4, delay: 0.73, ease: "easeOut" }}
+              >
+                <Link to={""}>
+                  <img src="/img/nav/room_search.webp" alt="" />
+                  <span>Salas</span>
+                </Link>
+              </motion.div>
+              <motion.div
+                className={styles.nav_btn}
+                initial={{ x: -15, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.4, delay: 0.84, ease: "easeOut" }}
+              >
+                <Link to={"/"}>
+                  <img src="/img/nav/game_search.webp" alt="" />
+                  <span>¿Que es?</span>
+                </Link>
+              </motion.div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
-      {/* <div className={style.header}>
-        <div className={style.nav_btn}>
-          <Link to={"/"}>
-            {" "}
-            <img src="/img/nav/start_search.webp" alt="" /> Inicio{" "}
-          </Link>
+      <div className={styles.btn_header} onClick={HandleBtnHeader}>
+        <div
+          className={styles.btn_header_animation}
+          style={{
+            transform: `rotate(${btnClickHeader == 1 ? "0" : "180deg"})`,
+          }}
+        >
+          <img src="/img/UI/btn/btn_arrow_pixel.png" alt="" />
         </div>
-        <div className={style.nav_btn}>
-          <Link to={"/"}>
-            <img src="/img/nav/game_search.webp" alt="" /> ¿De que trata?{" "}
-          </Link>
-        </div>
-        <div className={style.nav_btn}>
-          <Link to={"/Items"}>
-            <img src="/img/nav/item_search.webp" alt="" />
-            Items{" "}
-          </Link>
-        </div>
-        <div className={style.nav_btn}></div>
-        <div className={style.nav_btn}>
-          <Link to={""}>
-            <img src="/img/nav/enemie_search.webp" alt="" />
-            Enemigos
-          </Link>
-        </div>
-        <div className={style.nav_btn}>
-          <Link to={""}>
-            <img src="/img/nav/character_search.webp" alt="" />
-            Personajes
-          </Link>
-        </div>
-        <div className={style.nav_btn}>
-          <Link to={""}>
-            <img src="/img/nav/room_search.webp" alt="" />
-            Salas
-          </Link>
-        </div>
-      </div> */}
+      </div>
+      <AnimatePresence>
+        {btnClickHeader == 1 && (
+          <motion.div
+            className={styles.black_shadow}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            onClick={HandleBtnHeader}
+          ></motion.div>
+        )}
+      </AnimatePresence>
     </motion.header>
   );
 };
