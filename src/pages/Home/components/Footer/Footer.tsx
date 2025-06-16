81c6a01ae48722af89787c1b259718b01e363c82
Mijael Duart Yauyo Granados <yauyogranadosmijael@gmail.com>
2025-06-16 03:21:35 -0500

    feat(footer): agregando logo con sus estilos

--- src/pages/Home/components/Footer/Footer.tsx
+++ src/pages/Home/components/Footer/Footer.tsx
@@ -23,37 +23,53 @@ const Footer = ({ ScrollYProgress }: FooterProps) => {
         <motion.div
           className={styles.footer_home}
           initial={{ opacity: 0 }}
-          animate={{ opacity: 1, transition: { duration: 1, delay: 0.7 } }}
+          animate={{ opacity: 1, transition: { duration: 0.8 } }}
           exit={{ opacity: 0, transition: { duration: 0.25, delay: 0 } }}
         >
           <motion.div
             className={styles.img_footer_left}
-            initial={{ opacity: 0, x: -100 }}
+            initial={{ opacity: 0, x: -80 }}
             animate={{
               opacity: 1,
               x: 0,
-              transition: { duration: 0.55, delay: 0.6,ease:"easeOut" },
+              transition: { duration: 0.8, delay: 0.4, ease: "easeInOut" },
             }}
             exit={{
               opacity: 0,
               x: -200,
-              transition: { duration: 0.25, delay: 0,ease:"easeOut" },
+              transition: { duration: 0.4, delay: 0, ease: "easeInOut" },
             }}
           ></motion.div>
           <motion.div
             className={styles.img_footer_right}
-            initial={{ opacity: 0, x: 100 }}
+            initial={{ opacity: 0, x: 80 }}
             animate={{
               opacity: 1,
               x: 0,
-              transition: { duration: 0.55, delay: 0.6,ease:"easeOut"},
+              transition: { duration: 0.8, delay: 0.4, ease: "easeInOut" },
             }}
             exit={{
               opacity: 0,
               x: 200,
-              transition: { duration: 0.25, delay: 0,ease:"easeOut" },
+              transition: { duration: 0.4, delay: 0, ease: "easeInOut" },
             }}
           ></motion.div>
+          <motion.div
+            className={styles.logo}
+            initial={{ opacity: 0, top:-50 }}
+            animate={{
+              opacity: 1,
+              top: 0,
+              transition: { duration: 0.75, delay: 0.6, ease: "easeInOut" },
+            }}
+            exit={{
+              opacity: 0,
+              top: -50,
+              transition: { duration: 0.4, delay: 0, ease: "easeInOut" },
+            }}
+          >
+            <img src="/img/logo/Logo.webp"></img>
+          </motion.div>
         </motion.div>
       )}
     </AnimatePresence>
